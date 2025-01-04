6b45bfafe6e3a33116b01ad4eb13b634bf92a044
gbz3 <1808483+gbz3@users.noreply.github.com>
2025-01-04 09:25:22 +0900

    success!

--- my-app/src/test/java/com/github/gbz3/try_jqwik_java/PropertyBasedTest.java
+++ my-app/src/test/java/com/github/gbz3/try_jqwik_java/PropertyBasedTest.java
@@ -2,17 +2,19 @@ package com.github.gbz3.try_jqwik_java;
 
 import net.jqwik.api.ForAll;
 import net.jqwik.api.Property;
+import net.jqwik.api.constraints.IntRange;
+import net.jqwik.api.constraints.NotEmpty;
 import org.assertj.core.api.*;
 
 public class PropertyBasedTest {
 
     @Property
-    boolean alwaysPositive(@ForAll int anInteger) {
+    boolean alwaysPositive(@ForAll @IntRange(min = Integer.MIN_VALUE + 1) int anInteger) {
         return Math.abs(anInteger) >= 0;
     }
 
     @Property
-    void checkLength(@ForAll String s1, @ForAll String s2) {
+    void checkLength(@ForAll @NotEmpty String s1, @ForAll @NotEmpty String s2) {
         final var str = s1 + s2;
         Assertions.assertThat(str.length()).isGreaterThan(s1.length());
         Assertions.assertThat(str.length()).isGreaterThan(s2.length());
